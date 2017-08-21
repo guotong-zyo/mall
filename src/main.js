86511c7442c6ae1b5a610a93514a0e85ed354fac
guotong <guotong159@gmail.com>
2017-08-21 17:57:06 +0800

    完成前端部分 增加懒加载效果

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyLoad from 'vue-lazyload'
 
 // 加入全局样式
 import '@/assets/css/base.css'
@@ -10,6 +11,10 @@ import '@/assets/css/product.css'
 import '@/assets/css/login.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+    loading: '/static/loading-svg/loading-spokes.svg'
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -15,27 +15,18 @@
                             <use xlink:href="#icon-arrow-short"></use>
                         </svg>
                     </a>
-                    <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+                    <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop()">Filter by</a>
                 </div>
                 <div class="accessory-result">
                     <!-- filter -->
-                    <div class="filter stopPop" id="filter">
+                    <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
                         <dl class="filter-price">
                             <dt>Price:</dt>
                             <dd>
-                                <a href="javascript:void(0)">All</a>
+                                <a href="javascript:void(0)" :class="{'cur':priceChecked=='all'}" @click="setPriceFilter('all')">All</a>
                             </dd>
-                            <dd>
-                                <a href="javascript:void(0)">0 - 100</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">100 - 500</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">500 - 1000</a>
-                            </dd>
-                            <dd>
-                                <a href="javascript:void(0)">1000 - 2000</a>
+                            <dd v-for="(price,index) in priceFilter" :key="index">
+                                <a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{'cur':priceChecked==index}"> {{price.startPrice}} - {{price.endPrice}}</a>
                             </dd>
                         </dl>
                     </div>
@@ -43,10 +34,9 @@
                     <div class="accessory-list-wrap">
                         <div class="accessory-list col-4">
                             <ul>
-                                <li v-for="item in goodsList" :key="item" :data-id="item.productId">
+                                <li v-for="(item,index) in goodsList" :data-id="item.productId" :key="index">
                                     <div class="pic">
-                                        <a href="#">
-                                            <img :src="'/static/img/' + item.prodcutImg" alt=""> </a>
+                                        <a href="#"> <img v-lazy="'/static/img/' + item.prodcutImg" alt=""> </a>
                                     </div>
                                     <div class="main">
                                         <div class="name">{{item.productName}}</div>
@@ -62,6 +52,7 @@
                 </div>
             </div>
         </div>
+        <div class="md-overlay" v-show="overLayFlag" @click="closeFilterPop"></div>
         <mall-Footer></mall-Footer>
     </div>
 </template>
@@ -77,15 +68,44 @@
         name: 'GoodList',
         data () {
             return {
-                msg: 'aaa',
                 goodsList: [],
+                priceFilter: [
+                    {
+                        startPrice: '0.00',
+                        endPrice: '500.00'
+                    },
+                    {
+                        startPrice: '500.00',
+                        endPrice: '1000.00'
+                    },
+                    {
+                        startPrice: '1000.00',
+                        endPrice: '2000.00'
+                    }
+                ],
+                priceChecked: 'all',
+                filterBy: false,
+                overLayFlag: false
             }
         },
         methods: {
             getGoodsList () {
                 axios.get('/api/goods').then((json) => {
-                    this.goodsList = json.data.result;
+                    this.goodsList = json.data.result
                 })
+            },
+            showFilterPop () {
+                this.filterBy = true
+                this.overLayFlag = true
+            },
+            closeFilterPop () {
+                this.filterBy = false
+                this.overLayFlag = false
+            },
+            setPriceFilter (index) {
+                this.priceChecked = index
+                this.filterBy = false
+                this.overLayFlag = false
             }
         },
         mounted () {
@@ -100,5 +120,5 @@
 </script>
 
 <style>
-    
+
 </style>
